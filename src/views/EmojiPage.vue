4e0f0fcb25cddfb510917a3e1317b3061ed7076b
Jake <110341554+os-hi@users.noreply.github.com>
2024-04-24 02:13:36 +0800

    added emoji page and functionality

--- src/views/EmojiPage.vue
+++ src/views/EmojiPage.vue
@@ -1,12 +1,19 @@
-<style scoped>
-  .example-content {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    height: 100%;
-  }
-</style>
 
+<script setup lang="ts">
+  import { IonHeader, IonToolbar, IonTitle, IonContent, IonPage } from '@ionic/vue';
+  import { ref as databaseRef, set } from 'firebase/database'
+  import {db} from '../database/firebase'
+
+
+
+  const handleActionClick = (emoji: string) => {
+
+  set(databaseRef(db, 'devices/' + "device1"), {
+      display: emoji
+  });
+}
+
+</script>
 <template>
   <ion-page>
     <ion-header>
@@ -15,11 +22,48 @@
       </ion-toolbar>
     </ion-header>
     <ion-content>
-      <div class="example-content">Emoji</div>
+      <div class="example-content">
+        <div class="buttons">
+          <button @click="handleActionClick('❤️')">
+            ❤️
+          </button>
+          <button @click="handleActionClick('😆')">
+            😆
+          </button>
+          <button @click="handleActionClick('😮')">
+            😮
+          </button>
+          <button @click="handleActionClick('😥')">
+            😥
+          </button>
+          <button @click="handleActionClick('😠')">
+            😠
+          </button>
+          <button @click="handleActionClick('👍')">
+            👍
+          </button>
+        </div>
+        
+      </div>
     </ion-content>
   </ion-page>
 </template>
 
-<script setup lang="ts">
-  import { IonHeader, IonToolbar, IonTitle, IonContent, IonPage } from '@ionic/vue';
-</script>
+<style scoped>
+  .example-content {
+    display: grid;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+  }
+  .buttons{
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    gap: 10px;
+  }
+  button{
+    border-radius: 1rem;
+    padding: .5rem .1rem;
+    font-size: 3.5rem;
+  }
+</style>
