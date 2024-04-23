d0c9f05daaea71ea9b8115819b9e20e8019692b6
Jake <110341554+os-hi@users.noreply.github.com>
2024-04-24 02:13:26 +0800

    added datepage and functionality

--- src/views/DatePage.vue
+++ src/views/DatePage.vue
@@ -1,11 +1,22 @@
-<style scoped>
-  .example-content {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    height: 100%;
+<script setup lang="ts">
+  import { IonHeader, IonToolbar, IonTitle, IonContent, IonPage } from '@ionic/vue';
+  import { ref as databaseRef, set } from 'firebase/database'
+  import {db} from '../database/firebase'
+
+  const handleActionClick = (data:string) => {
+    const now = new Date()
+    let submit = ""
+    if(data == "Date"){
+      submit = now.getMonth() + "/" + now.getDate() + "/" + now.getFullYear()
+    }else{
+      submit = now.getHours() + ":" + now.getMinutes()
+    }
+    set(databaseRef(db, 'devices/' + "device1"), {
+        display: submit
+    });
+    console.log(submit)
   }
-</style>
+</script>
 
 <template>
   <ion-page>
@@ -15,12 +26,32 @@
       </ion-toolbar>
     </ion-header>
     <ion-content>
-      <div class="example-content">Date</div>
+      <div class="example-content">
+        <div class="buttons">
+          <button @click="handleActionClick('Date')">Date</button>
+          <button @click="handleActionClick('Time')">Time</button>
+        </div>
+      </div>
+      
     </ion-content>
   </ion-page>
 </template>
 
-<script setup lang="ts">
-  import { IonHeader, IonToolbar, IonTitle, IonContent, IonPage } from '@ionic/vue';
-
-</script>
+<style scoped>
+  .example-content {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+  }
+  .buttons{
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+  }
+  button{
+    width: 15rem;
+    padding: 1rem;
+    font-size: 1rem;
+  }
+</style>
